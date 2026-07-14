2e39f155ae2484979fa6eb8768d25c7dc47d74a1
João Costa <jjuniorc@gmail.com>
2026-07-14 01:15:55 -0300

    Initial baseline commit

---
+++ src/main/java/org/omnikron/aimda/AimdaApplication.java
@@ -0,0 +1,13 @@
+package org.omnikron.aimda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AimdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AimdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/omnikron/aimda/AimdaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.omnikron.aimda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AimdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
